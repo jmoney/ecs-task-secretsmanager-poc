73e5cbcfad380a434e15270af83ceb3313c6f04b
J$ <jon.monette@gmail.com>
2023-12-01 22:14:09 -0600

    print more data

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"os"
 	"log"
+	"os"
 )
 
 var (
-	secret = os.Getenv("SECRET")
-	ilog = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	secret        = os.Getenv("SECRET")
+	environSecret = os.Getenv("ENVIRONMENT_SECRET")
+	ilog          = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 )
 
 func main() {
-	ilog.Printf("Secret: %s\n", secret)
+	ilog.Printf("Environment Secret: %s, Decrypted Secret: %s\n", environSecret, secret)
 }
